f4f655e6332c3a3f8e62217046e2819dc5111bbb
Manh <demacia152003@gmail.com>
2025-04-02 21:42:20 +0700

    sua cau truc trang

--- src/pages/LandingPage/Products.jsx
+++ src/pages/LandingPage/Products.jsx
@@ -1,7 +1,20 @@
+import Contact from "@/components/LandingPage/Contact";
+import Footer from "@/components/LandingPage/Footer";
+import Header from "@/components/LandingPage/Header";
+import Navbar from "@/components/LandingPage/Navbar";
+import ProductsPage from "@/components/LandingPage/ProductsPage";
 import React from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <Navbar />
+      <ProductsPage />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default Products;
