6fca4dde37f1216e8f4baea1d75a733aa64e7eb2
Manh <demacia152003@gmail.com>
2025-04-03 22:09:02 +0700

    chinh sua cac the

--- src/components/LandingPage/NewsCard.jsx
+++ src/components/LandingPage/NewsCard.jsx
@@ -4,24 +4,41 @@ const NewsCard = ({ id, title, date, image, excerpt, link }) => {
   return (
     <div
       key={id}
-      className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300"
+      className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 max-w-xs h-[380px] relative"
     >
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="h-52 overflow-hidden">
+        {/* Hình ảnh */}
+        <div className="h-36 overflow-hidden">
           <img
             src={image}
             alt={title}
             className="w-full h-full object-cover transition-transform duration-500 hover:scale-105"
           />
         </div>
-        <div className="p-5">
-          <h3 className="text-lg font-medium text-gray-800 hover:text-blue-600 transition-colors duration-300">
-            {title}
-          </h3>
-          <p className="text-sm text-gray-500 mt-2">{date}</p>
-          <p className="text-gray-600 mt-2">{excerpt}</p>
-          <p className="mt-3 text-blue-600 font-semibold">Đọc thêm →</p>
+
+        {/* Nội dung */}
+        <div className="p-5 flex flex-col justify-between">
+          <div className="mb-4">
+            {/* Tiêu đề với tối đa 2 dòng */}
+            <h3 className="text-lg font-semibold text-gray-800 hover:text-blue-600 transition-colors duration-300 line-clamp-2">
+              {title}
+            </h3>
+            {/* Ngày */}
+            <p className="text-sm text-gray-500 mt-1">{date}</p>
+          </div>
+
+          {/* Mô tả trong một div và cắt nội dung khi dài */}
+          <div className="overflow-hidden">
+            <p className="text-gray-600 mb-6 text-ellipsis line-clamp-3">
+              {excerpt}
+            </p>
+          </div>
         </div>
+
+        {/* Phần "Đọc thêm" cố định ở góc trái dưới */}
+        <p className="absolute bottom-4 left-6 text-blue-600 font-semibold text-sm">
+          Đọc thêm →
+        </p>
       </a>
     </div>
   );
